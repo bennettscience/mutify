c93df48a4a6b65b9f5e9859d6ac9c96d792a9d1c
bennettscience <brian.bennett2@gmail.com>
2021-10-27 14:34:27 -0400

    v 0.3
    Spotify changes the footer element class frequently. Did a tiny refactor
    to check for known classes on ads. When a known class is found, the tab
    is muted.

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,11 +1,20 @@
 setInterval(checkAdPlaying, 5000);
 
+const footerClasses = [ 
+    `footer[data-testid='now-playing-bar-ad-type-ad']`, 
+    `footer[data-testadtype='ad-type-ad']`
+]
+
 function checkAdPlaying() {
-    let el = document.querySelector("footer[data-testid='now-playing-bar-ad-type-ad']")
-    if(el) {
-        muteTab()
-    } else {
-        unmuteTab()
+    // perform several checks because spotify likes to fuss with their class names.
+    for(let adClass of footerClasses) {
+        let el = document.querySelector(adClass)
+        if(el) {
+            muteTab();
+            break;
+        } else {
+            unmuteTab()
+        }
     }
 }
 
